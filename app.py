3fbfcec2cbd798f41c0deaa1f185485893d2a39b
madi3384 <123278687+madi3384@users.noreply.github.com>
2025-03-31 19:51:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World from Mathew Di Stadio in 3308'
 
-@app.route('/db-test')
+@app.route('/db_test')
 def test():
     conn = psycopg2.connect ("postgresql://basketball_nmkt_user:vuU6ZLrvQdrqzxcVtVTJEVG4LT6xhWgl@dpg-cvlihe7gi27c73e3qco0-a/basketball_nmkt")
     conn.close()
