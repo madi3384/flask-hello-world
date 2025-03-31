76be92ecb6f8f3f3723fff2f7e5c9f0209eb8271
madi3384 <123278687+madi3384@users.noreply.github.com>
2025-03-31 19:49:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+import psycopg2
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World from Mathew Di Stadio in 3308'
+
+@app.route('/db-test')
+def test():
+    conn = psycopg2.connect ("postgresql://basketball_nmkt_user:vuU6ZLrvQdrqzxcVtVTJEVG4LT6xhWgl@dpg-cvlihe7gi27c73e3qco0-a/basketball_nmkt")
+    conn.close()
+    return "Database Connection Successful"
