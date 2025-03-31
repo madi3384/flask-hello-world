1fce880acd44ca9a1193b05597aefc2d18fe3425
madi3384 <123278687+madi3384@users.noreply.github.com>
2025-03-31 19:59:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,20 @@ def test():
     conn = psycopg2.connect ("postgresql://basketball_nmkt_user:vuU6ZLrvQdrqzxcVtVTJEVG4LT6xhWgl@dpg-cvlihe7gi27c73e3qco0-a/basketball_nmkt")
     conn.close()
     return "Database Connection Successful"
+
+@app.route('/db_create')
+def creation():
+    conn = psycopg2.connect ("postgresql://basketball_nmkt_user:vuU6ZLrvQdrqzxcVtVTJEVG4LT6xhWgl@dpg-cvlihe7gi27c73e3qco0-a/basketball_nmkt")
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball (
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+        ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table successfully created"
