704944a24ebd15caddbc7ad71d44e7fc35a4f99b
madi3384 <123278687+madi3384@users.noreply.github.com>
2025-03-31 20:06:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -27,4 +27,20 @@ def creation():
         ''')
     conn.commit()
     conn.close()
-    return "Basketball Table successfully created"
+    return "Basketball Table Successfully Created"
+
+@app.route('/db_insert')
+def adding():
+    conn = psycopg2.connect ("postgresql://basketball_nmkt_user:vuU6ZLrvQdrqzxcVtVTJEVG4LT6xhWgl@dpg-cvlihe7gi27c73e3qco0-a/basketball_nmkt")
+    cur = conn.cursor()
+    cur.execute('''
+        INSERT INTO Basketball (First, Last, City, Name, Number)
+        Values
+        ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+        ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+        ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+        ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+        ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Populated"
